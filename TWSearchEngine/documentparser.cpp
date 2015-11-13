26d90c7f5263f5df0deda8c5d4e88fa63547d4e3
henrysdev <henrysdev@gmail.com>
2015-11-13 16:50:32 -0600

    successfully able to parse using rapidxml. Next is label heads and the meat of the parsing problem

--- TWSearchEngine/documentparser.cpp
+++ TWSearchEngine/documentparser.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <iostream>
 //#include "rapidxml_iterators.hpp"
-#include "rapidxml_print.hpp"
+//#include "rapidxml_print.hpp"
 #include "rapidxml.hpp"
 #include "rapidxml_utils.hpp"
 #include <string>
@@ -21,19 +21,18 @@ void DocumentParser::ReadInXML(char *fileName)
 {
     std::string toStr = std::string(fileName);
     xmlName = toStr;
-    xmlStream.open(fileName);
-    std::ifstream t(xmlName);
-    std::string str((std::istreambuf_iterator<char>(t)),std::istreambuf_iterator<char>());
-    fullText = str;
     Parse();
 }
 
 void DocumentParser::Parse()
 {
     int x = 0;
-    rapidxml::file<> xmlFile(fullText.c_str());
+    //const char* cText = fullText.c_str();
+    rapidxml::file<> xmlFile(xmlName.c_str());
     rapidxml::xml_document<> doc;
-    //doc.parse<0>(xmlFile.data());
+    doc.parse<0>(xmlFile.data());
+    rapidxml::xml_node<>* firstNode = doc.first_node();
+    std::cout<<"first node: "<<firstNode->first_attribute()->value()<<std::endl;
     //rapidxml::xml_node<> * firstNode = doc.first_node();
 //    rapidxml::xml_document<> doc;
 //    doc.parse<0>(xmlFile.data());
