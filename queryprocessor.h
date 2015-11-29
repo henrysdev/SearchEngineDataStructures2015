a00c74573364a8d463601ad63d9903f04a093fe1
terrabl <aterra@smu.edu>
2015-11-28 21:51:11 -0600

    updated query processor.h

--- queryprocessor.h
+++ queryprocessor.h
@@ -1,24 +1,40 @@
-//#ifndef QUERYPROCESSOR_H
-//#define QUERYPROCESSOR_H
+#ifndef QUERYPROCESSOR_H
+#define QUERYPROCESSOR_H
 
+
+#include "documentparser.h"
+#include "avl_tree.h"
+#include "stemandstopremoval.h"
+#include "index.h"
+#include "porter2_stemmer.h"
+
+#include <iostream>
+#include <vector>
 #include <string>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <cstdio>
+#include <utility>
 
 class QueryProcessor
 {
 public:
     QueryProcessor();
-    vector<string> userQuery(string);
-    vector<string>  rank(HashTable<string, int>);
-    void createAVLIndex();
-    void createHashTableIndex();
-    void changeIndex(int);
-    void indexFile(string);
-    void clearIndex();
-    void outputIndex();
-    void correctCommand();
-
+    void query(DocumentParser* docParser);
+    bool isCommand(std::string);
+    std::vector<int> AND(std::vector<int>*, std::vector<int>*);
+    std::vector<int> OR(std::vector<int>*, std::vector<int>*);
+    std::vector<int> NOT(std::vector<int>*, std::vector<int>*);
+    std::vector<int> AND2(std::vector<int>*, std::vector<int>*, std::vector<int>*);
+    std::map<int,int> ANDnot(std::vector<int>*, std::vector<int>*);
+    std::vector<int> NOTand(std::map<int, int> , std::vector<int>*);
+    std::vector<int> rank(std::map<int, int>);
 private:
-    string booleanCommands[3];
+    std::string booleanCommands[3];
+   // std::vector<int>* final;
+
 };
 
-//#endif // QUERYPROCESSOR_H
+#endif // QUERYPROCESSOR_H
+
